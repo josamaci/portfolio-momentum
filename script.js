8ba42728ca6b4a3aa89436b8a8913efc29e8e8a1
josamaci <josemaciasag@gmail.com>
2024-05-27 10:58:15 -0500

    - avances

---
+++ script.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+    $("#menu-toggle").click(function (e) {
+        e.preventDefault();
+        $("#wrapper").toggleClass("toggled");
+    });
+
+    $('#dark-mode-toggle').change(function (e) {
+        e.preventDefault();
+        $("body").toggleClass("dark-mode");
+        $("#sidebar-wrapper").toggleClass("dark-mode");
+        $(".list-group-item").toggleClass("dark-mode");
+        $(".dropdown-menu").toggleClass("dark-mode");
+        $("nav").toggleClass("dark-mode");
+    });
+});
